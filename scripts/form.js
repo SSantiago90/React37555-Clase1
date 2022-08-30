898fdfc2dcf15ce613c33f3b18890a00362d5355
ssantiago90 <salkin.santiago@gmail.com>
2022-08-30 02:26:07 -0300

    setup desafio v2

--- scripts/form.js
+++ scripts/form.js
@@ -1,23 +1,19 @@
-console.log('loaded script form.js');
+console.log(
+  "%cScript form.js importado 👏",
+  "background: purple; color : white"
+);
 
-window.onload = function(){   
-    const miFormulario = ""/* obtengo un elemento del DOM */
-    // TODO: agrego un evento al elemento del DOM
-    /* miFormulario... ? */
-}
+window.onload = function () {
+  // TODO: Agregar un "event listener" a un evento al elemento del DOM
+  const miFormulario = "?"; /* obtengo un elemento del DOM */
+  /* miFormulario... ? */
+};
 
-function handleSubmitForm( event ){
-    // Javascript automáticamente recibe el parametro del evento
-    // el "target" de el evento que se desencadenó, es el elemento html <form> que clickeamos
-    console.log(event.target);    
-
-    // los forms guardan como un array sus inputs internos
-    // event.target[0] -> input nombre... event.target[1] -> input contraseña...  etc
-    
-    event.preventDefault();
-
-    const palabras = event.target[0].value
-    //dividimos nuestro String en un array, separando por espacios
-    const arrayDePalabras = palabras.split(' ');
-    console.log("Array:", arrayDePalabras);
+function handleSubmitForm(event) {
+  // TODO2: Convertir el input en un array de caracteres
+  // Javascript automáticamente recibe el parametro del evento
+  // el "target" de el evento que se desencadenó, es el elemento del DOM donde se originó el evento
+  // los forms guardan como un array sus inputs internos
+  // event.target[0] -> input nombre... event.target[1] -> input contraseña...  etc
+  //dividimos nuestro String en un array, separando por espacios
 }
