ad20ec45979819845aaea19624bd71667e403888
ssantiago90 <salkin.santiago@gmail.com>
2022-08-30 12:11:46 -0300

    livecoding clase 1

--- scripts/form.js
+++ scripts/form.js
@@ -3,17 +3,25 @@ console.log(
   "background: purple; color : white"
 );
 
-window.onload = function () {
-  // TODO: Agregar un "event listener" a un evento al elemento del DOM
-  const miFormulario = "?"; /* obtengo un elemento del DOM */
-  /* miFormulario... ? */
-};
+// TODO: Agregar un "event listener" a un evento al elemento del DOM
+/* obtengo un elemento del DOM */
+const miFormulario = document.querySelector("#loginForm");
+console.log(miFormulario);
+
+/* Agregamos un event listener al formulario de login */
+miFormulario.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  console.log(evt.target);
+});
+
+/* Mostrar por consola el username y contraseña */
+
+/* SI usuario="coder" Y contraseña="37555" -> mostrar formulario 2 */
 
 function handleSubmitForm(event) {
   // TODO2: Convertir el input en un array de caracteres
   // Javascript automáticamente recibe el parametro del evento
   // el "target" de el evento que se desencadenó, es el elemento del DOM donde se originó el evento
-  // los forms guardan como un array sus inputs internos
-  // event.target[0] -> input nombre... event.target[1] -> input contraseña...  etc
-  //dividimos nuestro String en un array, separando por espacios
+  /* Obtenemos el "input" ingresado por el usuario  */
+  /*  dividimos nuestro String en un array, separando por espacios y lo imprimimos en consola */
 }
